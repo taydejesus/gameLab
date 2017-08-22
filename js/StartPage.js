df439a7cdbd3f910f99c6641c6f52fe676fdff06
pvq576 <taylor.dejesus@capitalone.com>
2017-08-22 16:04:41 -0400

    added input fields to StartPage setup function

--- js/StartPage.js
+++ js/StartPage.js
@@ -1,18 +1,32 @@
 StartPage = (function(){
   return {
+    createInput: function(){
+      $('.container').append($('<p>').text('Optional: input number of rows and columns'));
+      var form = $('<form>');
+      var rows = $('<input>').attr('type', 'text');
+      var cols = $('<input>').attr('type', 'text');
+
+      form.append("Number of rows: ").append(rows);
+      form.append("Number of columns: ").append(cols);
+      $('.container').append(form);
+    },
+
     createStartButton: function(){
       var startButton = $('<button>').attr('type', 'submit').addClass('startButton');
       startButton.text('Start');
       //start game on click
-      startButton.on('click', ()=>App.setup(App.numberOfRows, App.numberOfColumns));
+      startButton.on('click', ()=>App.setup(App.numberOfRows, App.numberOfColumns)); //TODO change to get inputs
       $('.container').append(startButton);
     },
+
     printInstructions: function(){
       $('.container').append($('<h3>').addClass('instruction-header').text('Instructions'));
       var instructionText = "The numbers that label the rows and columns let you know how many 'bad' blocks are in that respective row or column. Click all the 'good' blocks to win!";
       $('.container').append($('<p>').addClass('instruction-text').text(instructionText));
     },
+
     setup: function(){
+      this.createInput();
       this.createStartButton();
       this.printInstructions();
     }
